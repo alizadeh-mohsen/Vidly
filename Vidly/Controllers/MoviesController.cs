9b44fd4d689fdcb1272babc7967ca17e9339b47b
Mohsen <kelarens@gmail.com>
2019-10-10 18:11:35 +0330

    Adding movie model and movie controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            Movie movie = new Movie()
+            {
+                Id = 1,
+                Name = "Avengers"
+            };
+            return View(movie);
+        }
+    }
+}
