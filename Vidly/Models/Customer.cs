01ae426d9d508573fdaf9affb9bc5ce3ad8dec81
Mohsen <kelarens@gmail.com>
2019-10-10 21:45:28 +0330

    Adding Migration

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,6 +9,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
