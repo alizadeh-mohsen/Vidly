ead45b6667ad1cf8eef022ce3a8ae5cf977c3c44
Mohsen <kelarens@gmail.com>
2019-10-18 08:55:38 +0330

    Customer validation

---
+++ Vidly/Models/AgeCheckValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class AgeCheckValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            if (customer.MembershipTypeId.Equals(0) || customer.MembershipTypeId.Equals(1))
+            {
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthday != null && DateTime.Now.Year - customer.Birthday.Year < 18)
+            {
+                return new ValidationResult("You must be above 18");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,12 +9,18 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(255)]
         public string Name { get; set; }
+
         public MembershipType MembershipType { get; set; }
+        
         public byte MembershipTypeId { get; set; }
+
+        [AgeCheckValidation]
         public DateTime Birthday { get; set; }
+
         public byte DiscountRate { get; set; }
     }
 }
